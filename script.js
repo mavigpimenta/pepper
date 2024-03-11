ea9579722b6004cd49364ea43e9072281c54ba7c
mavi <spotifymaria2607@gmail.com>
2024-03-11 16:59:20 -0300

    teste

---
+++ script.js
@@ -0,0 +1,36 @@
+let navButtons = document.querySelectorAll('.x');
+let sections = document.querySelectorAll('section');
+
+console.log(navButtons);
+console.log(sections);
+
+navButtons.forEach((link, index) => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        sections[index].scrollIntoView({behavior: 'smooth'});
+    })
+})
+
+function toggleActive(event) {
+    event.preventDefault();
+    var links = document.querySelectorAll('.nav-bar a');
+    links.forEach(function(link) {
+      link.classList.remove('active');
+    });
+    event.target.classList.add('active');
+}
+
+let buttonOne = document.querySelector('.btn1');
+let buttonTwo = document.querySelector('.btn2');
+let contact = document.querySelector('.contact');
+let weare = document.querySelector('.quem-somos');
+
+buttonOne.addEventListener('click', function(event) {
+    event.preventDefault();
+    contact.scrollIntoView({behavior: 'smooth'});
+})
+
+buttonTwo.addEventListener('click', function(event) {
+    event.preventDefault();
+    weare.scrollIntoView({behavior: 'smooth'});
+})
